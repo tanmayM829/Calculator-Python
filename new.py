5c2d64b80dcf5b471b4c62d9f8cecab52f9c73f6
Tanmay Mangle <tanmaymangle@gmail.com>
2026-04-05 14:42:06 +0530

    main function

--- new.py
+++ new.py
@@ -1,50 +1,34 @@
-from tkinter import *
 import flet as ft
 
-#--------------DECLARATION---------------
-top = Tk()
-top.geometry("400x400")
 
-marginX = 53
-marginY = 56
-expr = ""
-txt = StringVar()
+def main(page : ft.Page):
+    page.title = "Calculator App"
+    page.window.height = 400
+    page.window.width = 400
 
-myLabel = Label(top, textvariable=txt, bg='white', bd=4, height=3, width=29, relief=RIDGE, wraplength=150).place(x=0, y=0)
+    btns = [['del', 'AC', '%', "/"],
+            ['7', '8', '9', '*'],
+            ['4', '5', '6', '-'],
+            ['1', '2', '3', '+'],
+            ['+/-', '0', '.', '=']]
+    
+    buttons = []
+    for row in btns:
+        _row = []
+        for btn in row:
+            _row.append(ft.Button(
+                content=btn,
+                style=ft.ButtonStyle(
 
-btns = [["AC", "Del", "(", ")"],
-       [7, 8, 9, '÷'],
-       [4, 5, 6, '×'],
-       [1, 2, 3, '-'],
-       ['.', 0, '=', '+']
-]
+                )
+            ))
+        buttons.append(ft.Row(_row))
+    
+    my_stack = ft.Stack([ft.Column(buttons)])
 
-def insert(arg):
-    global expr
-    if arg in ["AC", "Del", "÷", "×", "="]:
-        if arg == "AC":
-            expr = ""
-        elif arg == "Del":
-            expr = expr[0: -1]
-        elif arg == "÷":
-            expr += "/"
-        elif arg == "×":
-            expr += "*"
-        elif arg == "=":
-            expr = str(calc())
-    else:
-        expr += str(arg)
-
-    #print(expr)
-    txt.set(expr)
-
-def calc():
-    return eval(expr)
-
-#TASK
-for i in range(5):
-    for j in range(4):
-        Button(top, text=f'{btns[i][j]}', fg='blue', bd=3, height=3, activebackground="grey", width=6, command=lambda i=i, j=j: insert(btns[i][j])).place(x=marginX*j, y=marginY*(i+1))
+    page.add(my_stack)
+    
 
 
-top.mainloop()
+
+ft.run(main)
